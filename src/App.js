430c9d15d2e13cd837b6b814625b1c154d102750
SamuelLucasVieira <samuellucasvieira.melo@gmail.com>
2025-05-04 14:52:20 -0300

    Teste

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 100 }}>
+      <h1>🚀 Deploy com Firebase Hosting</h1>
+      <p>Feito com React + CI/CD via GitHub Actions</p>
+    </div>
+  );
+}
+
+export default App;
